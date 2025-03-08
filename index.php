5997f3b08dcaec2bc6be5a0fb0ae3942a370cc9f
Sergio Garcia Butron <sergio.garciabutron@gmail.com>
2025-03-08 13:44:21 +0100

    Relleno la estructura HTML5 inicial del portal web público de la plataforma correplayas.

--- index.php
+++ index.php
@@ -3,12 +3,94 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portal web correplayas</title>
+    <title>Portal web Correplayas</title>
     <link rel="stylesheet" href="plataforma/estilos/portal.css">
 </head>
 <body>
-    <header></header>
-    <main></main>
-    <footer></footer>
+    <!-- Cabecera del sisito web -->
+    <header>
+        <!-- Logotipo del sitio web -->
+        <img src="" alt="logotipo del portal web correplayas"/>
+        <!-- Menú del sitio web -->
+        <ul>
+            <!-- Entrada del menu para concoer la plaatforma correplayas -->
+            <li><a herf="#correplayas">Correplayas</a></li>
+            <!-- Entrada de menu para saber más sobre los observatorios -->
+            <li><a herf="#observatorios">Observatorios</a></li>
+            <!-- Entrada del menu para saber más sobre el voluntariado -->
+            <li><a herf="#voluntariado">Voluntariado</a></li>
+            <!-- Entrada del menu para solicitar más información sobre el proyecto    -->
+            <li><a herf="#contacto">Contacto</a></li>
+            <!-- Entrada para volver al inicio  -->
+            <li><a href="#inicio"><span>Corre</span>playas.es</a></li>
+        </ul>
+    </header>
+    <!-- Contenidos del sitio web -->
+    <main id="inicio">
+        <!-- Sección para hablar de la plataforma correplayas -->
+         <section id="correplayas">
+            <!-- Artículo que describe el proyecto de la plataforma correplayas -->
+             <article>
+                <h1>Correplayas</h1>
+                <h2>¿Qué significa esta palabreja?</h2>
+                <p>Los correplayas es, en el habla popular de Cádiz, como se conoce al correlimos tridáctilo. Se trata de una pequeña ave limícola muy típica de las costas gaditanas, cuya velocidad corriendo por las orillas de nuestras playas para dar caza a su alimento da la sensación de levitar sobre la arena.</p>
+                <p>Este animal da nombre a nuestra asociación, forma nuestro logotipo y recoge toda la esencia de nuestro proyecto de ciencia ciudadana bajo la plataforma web que actualmente visitas. Nuestro proyecto consiste en censar aves a lo largo de nuestras costas y ricos espacios de marisma que nos brinda nuestra provincia. Los datos ayudan a la comunidad científica a tener una evaluación constante de la evolución de las especies en la zona.</p>
+                <p>La provincia de Cádiz es un enclave de importancia ornitológica a nivel internacional, debido a su ubicación estratégica en la ruta migratoria entre África y Europa, así como a la diversidad de sus hábitats.</p>
+                <h3 class="cta">¿Quieres conocer dónde censamos?</h3>
+                <a class="boton" href="#observatorios">Nuestros observatorios</a>
+            </article>
+        </section>
+         <!-- Seccion para hablar de los observatorios -->
+         <section id="observatorios">
+            <!-- Artículo que habla de nuestros observatorios y lo que aporta al proyecto -->
+            <article>
+                <h1>Los observatorios</h1>
+                <h2>¿Qué aportan al proyecto?</h2>
+                <p>Nuestra amplia red de observatorios constituye nuestras zonas estratégicas para la observación y censos de aves. Desde torres vigías del siglo XVI, donde se observaba la llegada de los temidos piratas berberiscos o del atún para la colocación de las almadrabas, un arte de pesca con mucha tradición en nuestra provincia.</p>
+                <p>Hasta el inhóspito y, a la vez, rico terreno de marismas y salinas abandonadas, que tanto auge tuvieron en épocas pasadas hasta la llegada de otros métodos de conservación de alimentos, vinculadas a la industria del salazón y hechos históricos como la invasión napoleónica o testigos de importantes batallas.</p>
+                <p>Vinculadas a la industria del salazón y hechos históricos como la invasión napoleónica o escenarios de importantes batallas.</p>
+                <h3 class="cta">¿Te apetece participar?</h3>
+                <a class="boton" href="#voluntariado">Nuestro voluntariado</a>
+            </article>
+        </section>
+         <!-- Seccion para hablar del voluntariado -->
+         <section id="voluntariado">
+            <!-- Artículo que habla de nuestro volutariado como potencial en la ciencia ciudadana -->
+            <article>
+                <h1>El voluntariado</h1>
+                <h2>¿Cómo puedes ayudar?</h2>
+                <p>En el corazón de nuestro proyecto de ciencia ciudadana reside la pasión y el compromiso de nuestros voluntarios. Son ellos, con su dedicación y entusiasmo, quienes hacen posible la recopilación de datos cruciales para la conservación de la biodiversidad en nuestra región. Cada observación, cada censo, cada hora invertida en el campo, contribuye a un conocimiento más profundo de nuestro entorno y a la toma de decisiones informadas para su protección.</p>
+                <p>Nuestros voluntarios son los ojos y oídos de la ciencia en el terreno. Desde la observación de aves migratorias en las marismas hasta el seguimiento de especies amenazadas en nuestros bosques, su labor es fundamental para comprender la dinámica de los ecosistemas y detectar posibles amenazas. Su participación activa no solo enriquece nuestro proyecto con datos valiosos, sino que también fomenta una mayor conciencia ambiental en la comunidad.</p>
+                <p>El voluntariado en nuestro proyecto es una experiencia enriquecedora que trasciende la mera recopilación de datos. Es una oportunidad para conectar con la naturaleza, aprender sobre la biodiversidad local y formar parte de una comunidad comprometida con su conservación. Juntos, voluntarios y científicos, construimos un futuro más sostenible, donde la ciencia y la ciudadanía se unen para proteger nuestro patrimonio natural.</p>
+                <h3 class="cta">¿Tienes alguna duda o pregunta?</h3>
+                <a class="boton" href="#contacto">Contáctanos</a>
+            </article>
+         </section>
+        <!-- Seccion para contactar con administradores de la plataforma -->
+         <section id="contacto">
+            <!-- Información introductoria al formualrio de contacto -->
+            <h1>El voluntariado</h1>
+            <h2>¿En qué podemos ayudarte?</h2>
+            <p>¡Nos encantaría saber de ti! Si tienes alguna pregunta, comentario o simplemente quieres ponerte en contacto con nosotros, rellena el siguiente formulario y te responderemos lo antes posible.</p>
+            <!-- Formulario de contacto de la plataforma -->
+            <!-- REVISARLO Y DEJARLO CONFIGURADO PARA HTML5  -->
+            <form id="contactenos" >
+                <label for="frm-email">Correo electrónico:</label>
+                <input type="email" name="frm-email" id="frm-email">
+                <label for="frm-nombre">Nombre y apellidos:</label>
+                <input type="text" name="frm-nombre" id="frm-nombre">
+                <label for="frm-mensaje">Mensajes:</label>
+                <textarea name="frm-mensaje" id="frm-mensaje" cols="30" rows="10"></textarea>
+                <button type="submit" class="button btn-destacado" value="Enviar">
+            </form>          
+         </section>
+    </main>    
+    <!-- Pie de página del sitio web -->
+    <footer>
+        <!-- Todos los derechos reservados -->
+         <small>Correplayas &#169;<?php echo date('Y'); ?> Todos los derechos reservados</small>
+         <small>Proyecto DAW2425 - I.E.S AGUADULCE</small>
+         <small>Sergio García Butrón</small>
+    </footer>
 </body>
 </html>
