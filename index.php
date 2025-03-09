7ede6d7d300c3a6c2dd6a50f8332df2c98ad70e3
Sergio Garcia Butron <sergio.garciabutron@gmailcom>
2025-03-09 11:38:10 +0100

    Cambios idea en fondos de secciones

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
                 <h1>Correplayas</h1>
                 <h2>¿Qué significa esta palabreja?</h2>
                 <p>Los correplayas es, en el habla popular de Cádiz, como se conoce al correlimos tridáctilo. Se trata de una pequeña ave limícola muy típica de las costas gaditanas, cuya velocidad corriendo por las orillas de nuestras playas para dar caza a su alimento da la sensación de levitar sobre la arena.</p>
+                <img src="plataforma/imagenes/correplayas.jpg" alt="Foto del correlimos común con triactilo al fondo" />
                 <p>Este animal da nombre a nuestra asociación, forma nuestro logotipo y recoge toda la esencia de nuestro proyecto de ciencia ciudadana bajo la plataforma web que actualmente visitas. Nuestro proyecto consiste en censar aves a lo largo de nuestras costas y ricos espacios de marisma que nos brinda nuestra provincia. Los datos ayudan a la comunidad científica a tener una evaluación constante de la evolución de las especies en la zona.</p>
                 <p>La provincia de Cádiz es un enclave de importancia ornitológica a nivel internacional, debido a su ubicación estratégica en la ruta migratoria entre África y Europa, así como a la diversidad de sus hábitats.</p>
                 <h3 class="cta">¿Quieres conocer dónde censamos?</h3>
@@ -47,6 +48,7 @@
                 <h1>Los observatorios</h1>
                 <h2>¿Qué aportan al proyecto?</h2>
                 <p>Nuestra amplia red de observatorios constituye nuestras zonas estratégicas para la observación y censos de aves. Desde torres vigías del siglo XVI, donde se observaba la llegada de los temidos piratas berberiscos o del atún para la colocación de las almadrabas, un arte de pesca con mucha tradición en nuestra provincia.</p>
+                <img src="plataforma/imagenes/observatorios.jpg" alt="Foto del observatorio Torre del Puerco" />
                 <p>Hasta el inhóspito y, a la vez, rico terreno de marismas y salinas abandonadas, que tanto auge tuvieron en épocas pasadas hasta la llegada de otros métodos de conservación de alimentos, vinculadas a la industria del salazón y hechos históricos como la invasión napoleónica o testigos de importantes batallas.</p>
                 <p>Vinculadas a la industria del salazón y hechos históricos como la invasión napoleónica o escenarios de importantes batallas.</p>
                 <h3 class="cta">¿Te apetece participar?</h3>
@@ -60,6 +62,7 @@
                 <h1>El voluntariado</h1>
                 <h2>¿Cómo puedes ayudar?</h2>
                 <p>En el corazón de nuestro proyecto de ciencia ciudadana reside la pasión y el compromiso de nuestros voluntarios. Son ellos, con su dedicación y entusiasmo, quienes hacen posible la recopilación de datos cruciales para la conservación de la biodiversidad en nuestra región. Cada observación, cada censo, cada hora invertida en el campo, contribuye a un conocimiento más profundo de nuestro entorno y a la toma de decisiones informadas para su protección.</p>
+                <img src="plataforma/imagenes/voluntariado.jpg" alt="Foto del correlimos común con triactilo al fondo" />
                 <p>Nuestros voluntarios son los ojos y oídos de la ciencia en el terreno. Desde la observación de aves migratorias en las marismas hasta el seguimiento de especies amenazadas en nuestros bosques, su labor es fundamental para comprender la dinámica de los ecosistemas y detectar posibles amenazas. Su participación activa no solo enriquece nuestro proyecto con datos valiosos, sino que también fomenta una mayor conciencia ambiental en la comunidad.</p>
                 <p>El voluntariado en nuestro proyecto es una experiencia enriquecedora que trasciende la mera recopilación de datos. Es una oportunidad para conectar con la naturaleza, aprender sobre la biodiversidad local y formar parte de una comunidad comprometida con su conservación. Juntos, voluntarios y científicos, construimos un futuro más sostenible, donde la ciencia y la ciudadanía se unen para proteger nuestro patrimonio natural.</p>
                 <h3 class="cta">¿Tienes alguna duda o pregunta?</h3>
