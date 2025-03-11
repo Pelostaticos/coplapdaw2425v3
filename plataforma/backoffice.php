ce2ea1ec09608f444c892f0dcb77f8478222e9d1
Sergio Garcia Butron <sergio.garciabutron@gmail.com>
2025-03-11 12:11:54 +0100

    Comienzo el desarrollo de laos entregables con funciones vitales de muy alta prioridad.

--- plataforma/backoffice.php
+++ plataforma/backoffice.php
@@ -1,5 +1,34 @@
 <?php
 
+/**
+ * [Descripción breve de la función, clase o archivo]
+ *
+ * [Descripción detallada, si es necesario, puede incluir ejemplos de uso]
+ *
+ * @category [Categoría del código, por ejemplo, "Database", "User", "API"]
+ * @package  [Nombre del paquete o namespace]
+ * @author   [Tu nombre o el nombre del equipo]
+ * @license  [Tipo de licencia, por ejemplo, MIT, GPL, etc.]
+ * @link     https://abdc.es/blog/documentos-externos-empresa-tipos/
+ * @version  [Número de versión]
+ * @since    [Versión en la que se introdujo el código]
+ *
+ * @param [Tipo] $[nombre] [Descripción del parámetro]
+ * @param [Tipo] $[nombre] [Descripción del parámetro]
+ * ...
+ *
+ * @return [Tipo] [Descripción del valor de retorno]
+ *
+ * @throws [Tipo de excepción] [Descripción de la excepción]
+ * @throws [Tipo de excepción] [Descripción de la excepción]
+ * ...
+ *
+ * @example
+ * [Ejemplo de uso del código]
+ *
+ * @deprecated [Descripción de por qué el código está obsoleto, si aplica]
+ */
+
     echo "Bienvenido al futuro enrutador de la Playaforma Correplayas...";
     echo "<br/>";
     echo "El comando que quieres ejecutar es: " . $_GET['comando'];
