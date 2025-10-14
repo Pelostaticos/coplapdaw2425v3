88298a55d767a59f11bf4177b072e225fb168762
Sergio Garcia Butron <sergio.garciabutron@gmail.com>
2025-10-14 13:51:14 +0200

    Paso la Variable de Control del modo deostracion a la plantilla del portal publico para que ajuste los botones de acceso según este configurado su estado

--- plataforma/nucleo/Core.php
+++ plataforma/nucleo/Core.php
@@ -50,6 +50,7 @@ class Core {
             // Hay un usuario logueado en la plataforma. Entonces: 
             // Asigno las variables de la plantilla para la página de inicio del backoffice
             $smarty->assign('usuario', Core::nombreUsuario());
+            $smarty->assign('is_demo_mode', IS_DEMO_MODE);
             $smarty->assign('anyo', date('Y'));
             // Muestro la plantilla de la página de inicio del backoffice
             $smarty->display('comunes/backoffice.tpl');
